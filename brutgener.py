21475d8c345e6aa181b0f59eb84cbed3f0c218d5
Igor Ivanov <31894518+IVIGOR13@users.noreply.github.com>
2019-07-09 23:41:14 +0300

    Add files via upload

--- brutgener.py
+++ brutgener.py
@@ -1,176 +1,254 @@
+#
+#
+#
 import sys
 import progressbar
+import time
+
 
 NAME = 'brutgener'
 
+SKIP_SYMBOL        = '_'
+
+list_word          = []
+out_name           = ''
+lengths            = [0, 99999999]
+elements           = [1, 1]
+is_pattern         = False
+pattern            = ''
+pat_index          = []
+position           = []
+out_file           = 0
+
+
 def print_help():
+    print('BRUTGENER')
+    print()
     print('Usage: \n\tpython3 {}.py [OPTION...] arguments\n'.format(NAME), sep='\n')
     print("""Options: \n """ +
-        """\t-l, --list <path_file>         select file with dictionary\n""" +
-        """\t-o, --out  <path_file>         select a file to display the result\n\n""" +
-        """\t-d, --dict=<dictionary with one-character words>   select dictionary without file\n""" + 
-        """\t-p, --pattern=<pattern: skip is indicated '_'>     select pattern words\n\n""" +
-        """\t--max-len=<value>              maximum word length\n""" +
-        """\t--min-len=<value>              minimum word length\n""" +
-        """\t--max-elem=<value>             maximum number of elements in a word\n""" +
-        """\t--min-elem=<value>             minimum number of elements in a word\n\n""" +
-        """\t-h, --help                     display this help and exit\n""", sep='\n')
-    print("""Example usage: \n\tpython3 {}.py --dict=abcdefg --pattern=_1_2_3_4 -o out.txt\n""".format(NAME) +
-        """\tpython3 {}.py -l list.txt -o out.txt --min-elem=5 --max-elem=8 --min-len=6 --max-len=13\n""".format(NAME), sep='\n')
+        """\t-l | --list <path_file>         select file with dictionary\n""" +
+        """\t-o | --out  <path_file>         select a file to display the result\n\n""" +
+        """\t-d | --dict=<dictionary with one-character words>   select dictionary without file\n""" +
+        """\t-p | --pattern=<pattern: skip is indicated '{}'>     select pattern words\n"""
+            .format(SKIP_SYMBOL) +
+        """\t--max-len=<value>               maximum word length\n""" +
+        """\t--min-len=<value>               minimum word length\n""" +
+        """\t--max-elem=<value>              maximum number of elements in a word\n""" +
+        """\t--min-elem=<value>              minimum number of elements in a word\n""" +
+        """\t-h | --help                     display this help and exit\n""", sep='\n')
+    print("""Example usage: \n""" +
+        """\tpython3 {}.py --dict=abcdefg --pattern={}1{}2{}3{}4 -o out.txt\n"""
+            .format(NAME, SKIP_SYMBOL, SKIP_SYMBOL, SKIP_SYMBOL, SKIP_SYMBOL) +
+        """\tpython3 {}.py -l list.txt -o out.txt --min-elem=5 --max-elem=8 --min-len=6 --max-len=13\n"""
+            .format(NAME) +
+        """\tpython3 {}.py -d=0123 --max-elem=7\n"""
+            .format(NAME)
+        , sep='\n')
     sys.exit(0)
 
-def follow(i):
+
+def follow_position(i, list_len):
+    global position
+
     if i == 0:
-        if pos[i] < list_len - 1:
-            pos[i] = pos[i] + 1
+        if position[i] < list_len - 1:
+            position[i] = position[i] + 1
+            return position
         else:
-            return False
+            return position
+
     else:
-        if pos[i] < list_len-1:
-            pos[i] = pos[i] + 1
+        if position[i] < list_len - 1:
+            position[i] = position[i] + 1
+            return position
         else:
-            pos[i] = 0
-            follow(i - 1)
-    return True
+            position[i] = 0
+            follow_position(i - 1, list_len)
+
 
 def get_word():
+    global list_word
+    global position
+
     word = ''
-    for symb in pos:
+    for symb in position:
         word += list_word[symb]
     return word
 
-list_word = []
-out_name = ''
 
-min_len, max_len = 0, 99999999
-min_elem, max_elem = 1, 1
+def print_word():
+    global is_pattern
+    global pattern
+    global pat_index
+    global position
+    global out_file
+    global lengths
 
-is_file = False
-is_pattern = False
-is_out = False
-voc = []
+    word = get_word()
 
-pat = '_'
-pattern = ''
-pat_len = 0
-pat_index = []
-
-
-arg = sys.argv
-
-if len(arg) == 1:
-    print_help()
-
-else:
-    for option in arg:
-        if option == '-h' or option == '--help':
-            print_help()
-
-        if option == '-l' or option == '--list':
-            file = arg[arg.index(option) + 1]
-            file = 'list.txt'
-            with open(file) as lines:
-                for line in lines:
-                    list_word.append(str(line.rstrip()))
-            is_file = True
-
-        elif option == '-o' or option == '--out':
-            is_out = True
-            out_name = arg[arg.index(option) + 1]
-
-        elif option.count('--max-len=') > 0:
-            max_len = int(arg[arg.index(option)].split('=')[1])
-
-        elif option.count('--min-len=') > 0:
-            min_len = int(arg[arg.index(option)].split('=')[1])
-
-        elif option.count('--max-elem=') > 0:
-            max_elem = int(arg[arg.index(option)].split('=')[1])
-
-        elif option.count('--min-elem=') > 0:
-            min_elem = int(arg[arg.index(option)].split('=')[1])
-
-        elif option.count('--dict=') > 0 or option.count('-d=') > 0:
-            voc = list(arg[arg.index(option)].split('=')[1])
-
-        elif option.count('--pattern=') > 0 or option.count('-p=') > 0:
-            is_pattern = True
-            pattern = '='.join(arg[arg.index(option)].split('=')[1:])
-            pat_len = pattern.count(pat)
-            pat_index = [i for i, w in enumerate(pattern) if w == '_']
-
-if not is_out:
-    print('You did not specify an output file')
-    sys.exit(0)
-
-if not is_file:
-    list_word = voc
-
-if is_pattern:
-    min_len, max_len = 0, 9999999
-    min_elem, max_elem = pat_len, pat_len
+    if (len(word) >= lengths[0] and len(word) <= lengths[1]):
+        if is_pattern:
+            word, string = list(word), list(pattern)
+            for index, j in enumerate(pat_index):
+                string[j] = word[index]
+            print(''.join(string), file=out_file)
+        else:
+            print(word, file=out_file)
+        return True
 
 
-list_word.sort()
-list_len = len(list_word)
+def main():
+    global list_word
+    global out_name
+    global position
+    global pattern
+    global pat_index
+    global lengths
+    global elements
+    global out_file
+    global SKIP_SYMBOL
+    global is_pattern
 
-if max_elem < min_elem:
-    max_elem = min_elem
+    specified_out      = False
+    is_out_in_console  = False
 
-pos =  []
+    progress = 0
+    was_written = 0
 
-value = 0
-for i in range(min_elem, max_elem + 1):
-    value += pow(list_len, i)
+    arg = sys.argv
 
-print('All words: {}'.format(value))
-print('Dictionary size: {}'.format(len(list_word)))
-print('Dictionary: \n\t{}'.format(', '.join(list_word)), sep='\n')
-if is_pattern:
-    print('Pattern: \n\t{}'.format(pattern), sep='\n')
-print('Range of word length: [{}, {}]'.format(min_len, max_len))
-print('Range of number of elements in a word: [{}, {}]'.format(min_elem, max_elem))
+    if not len(arg[1:]):
+        print_help()
 
-progress = 0
-val_written = 0
-if value > 0:
-    with progressbar.ProgressBar(max_value=int(value)) as bar:
-        with open(out_name, 'w') as out_file:
+    else:
+        for option in arg:
+            if option == '-h' or option == '--help':
+                print_help()
 
-            for run in range(min_elem-1, max_elem):
-                pos = [0 for pos in range(run + 1)]
+            if option == '-l' or option == '--list':
+                file = arg[arg.index(option) + 1]
+                file = 'list.txt'
+                with open(file) as lines:
+                    for line in lines:
+                        list_word.append(str(line.rstrip()))
 
-                word = get_word()
-                if len(word) >= min_len and len(word) <= max_len:
-                    if is_pattern:
-                            word = list(word)
-                            string = list(pattern)
-                            for index, j in enumerate(pat_index):
-                                string[j] = word[index]
-                            print(''.join(string), file=out_file)
-                    else:
-                        print(word, file=out_file)
-                    val_written += 1
+            elif option == '-o' or option == '--out':
+                specified_out = True
+                out_name = arg[arg.index(option) + 1]
+
+            elif option.count('--max-len=') > 0:
+                lengths[1] = int(arg[arg.index(option)].split('=')[1])
+
+            elif option.count('--min-len=') > 0:
+                lengths[0] = int(arg[arg.index(option)].split('=')[1])
+
+            elif option.count('--max-elem=') > 0:
+                elements[1] = int(arg[arg.index(option)].split('=')[1])
+
+            elif option.count('--min-elem=') > 0:
+                elements[0] = int(arg[arg.index(option)].split('=')[1])
+
+            elif option.count('--dict=') > 0 or option.count('-d=') > 0:
+                list_word = list(arg[arg.index(option)].split('=')[1])
+
+            elif option.count('--pattern=') > 0 or option.count('-p=') > 0:
+                is_pattern = True
+                pattern = '='.join(arg[arg.index(option)].split('=')[1:])
+                pat_index = [i for i, w in enumerate(pattern) if w == SKIP_SYMBOL]
+
+    if not specified_out:
+        print('You did not specify an output file')
+        nin = input('Print results to console: [y/n]: ')
+        if nin == 'y':
+            is_out_in_console = True
+        elif nin == 'n':
+            sys.exit(0)
+        else:
+            print('Unknow command')
+            sys.exit(0)
+
+    if is_pattern:
+        lengths = [0, 9999999]
+        elements = [pattern.count(SKIP_SYMBOL), pattern.count(SKIP_SYMBOL)]
+
+
+    list_word.sort()
+    list_len = len(list_word)
+
+    if elements[1] < elements[0]:
+        elements[1] = elements[0]
+
+    value = 0
+    size = 0
+    for i in range(elements[0], elements[1] + 1):
+        x = pow(list_len, i)
+        value += x
+        size += x * (i + 2)
+
+    print('Presumably words: {}'.format(value))
+    print('Dictionary size: {}'.format(len(list_word)))
+    print('Dictionary: \n\t{}'.format(', '.join(list_word)), sep='\n')
+    if is_pattern:
+        print('Pattern: \n\t{}'.format(pattern), sep='\n')
+    print('Range of word length: [{}, {}]'.format(lengths[0], lengths[1]))
+    print('Range of number of elements in a word: [{}, {}]'.format(elements[0], elements[1]))
+    print("""Size output file: \n\t{} byte\n\t{} KiB\n\t{} MiB\n\t{} GiB """
+        .format(size,
+            round(size/1024.0, 2),
+            round(size/1024.0/1024.0, 2),
+            round(size/1024.0/1024.0/1024.0, 2)
+        ), sep='\n')
+    print()
+
+    time.sleep(1)
+
+    if value > 0:
+
+        if not is_out_in_console:
+            out_file = open(out_name, 'w')
+
+            with progressbar.ProgressBar(max_value=value) as bar:
+                for run in range(elements[0] - 1, elements[1]):
+                    position = [0 for pos in range(run + 1)]
+
+                    if print_word():
+                        was_written += 1
                     progress += 1
                     bar.update(progress)
+
+                    for i in range(pow(list_len, run + 1) - 1):
+                        follow_position(run, list_len)
+                        if print_word():
+                            was_written += 1
+                        progress += 1
+                        bar.update(progress)
+        else:
+            out_file = sys.stdout
+
+            for run in range(elements[0] - 1, elements[1]):
+                position = [0 for pos in range(run + 1)]
+
+                if is_out_in_console:
+                    if print_word():
+                        was_written += 1
+                    progress += 1
 
                 for i in range(pow(list_len, run + 1) - 1):
-                    follow(run)
-                    word = get_word()
-                    if len(word) >= min_len and len(word) <= max_len:
-                        if is_pattern:
-                            word = list(word)
-                            string = list(pattern)
-                            for index, j in enumerate(pat_index):
-                                string[j] = word[index]
-                            print(''.join(string), file=out_file)
-                        else:
-                            print(word, file=out_file)
-                        val_written += 1
+                    follow_position(run, list_len)
+                    if print_word():
+                        was_written += 1
                     progress += 1
-                    bar.update(progress)
 
-    print('Total words: {}'.format(val_written))
-    print('Successfully written to file {}'.format(out_name))
-else:
-    print("Error: number of output words is zero.")
+
+
+        print()
+        print('Total words: {}'.format(was_written))
+        if not is_out_in_console:
+            print('Successfully written to file {}'.format(out_name))
+
+    else:
+        print("Error: number of output words is zero.")
+
+
+if __name__ == "__main__":
+    main()
